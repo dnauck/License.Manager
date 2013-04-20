71e27a3a27e28a67a257bd6e91142cfe07533590
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-20 12:47:20 +0200

    Start ServiceStack configuration on web startup

---
+++ src/License.Manager/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using License.Manager.Core;
+
+namespace License.Manager
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Request.IsLocal)
+                ServiceStack.MiniProfiler.Profiler.Start();
+        }
+
+        protected void Application_EndRequest(object src, EventArgs e)
+        {
+            ServiceStack.MiniProfiler.Profiler.Stop();
+        }
+    }
+}
