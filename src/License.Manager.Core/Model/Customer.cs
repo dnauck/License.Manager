c2e073e93b8097661c3ce7743b1b81a63b66171d
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-20 13:00:15 +0200

    Add customer entity

---
+++ src/License.Manager.Core/Model/Customer.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.ServiceHost;
+
+namespace License.Manager.Core.Model
+{
+    [Route("/customers", "POST")]
+    [Route("/customers/{Id}", "PUT, DELETE")]
+    [Route("/customers/{Id}", "GET, OPTIONS")]
+    public class Customer : EntityBase
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+    }
+}
