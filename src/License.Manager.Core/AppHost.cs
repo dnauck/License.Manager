72bb26a557a52b6b952bd4979d5107f8e02880e2
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-20 12:44:27 +0200

    Add ServiceStack configuration

---
+++ src/License.Manager.Core/AppHost.cs
@@ -0,0 +1,95 @@
+﻿using System.Web;
+using Funq;
+using Raven.Client;
+using Raven.Client.Indexes;
+using ServiceStack.Authentication.RavenDb;
+using ServiceStack.Configuration;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Auth;
+using ServiceStack.ServiceInterface.Cors;
+using ServiceStack.ServiceInterface.Validation;
+using ServiceStack.WebHost.Endpoints;
+using ServiceStack.WebHost.Endpoints.Extensions;
+
+namespace License.Manager.Core
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() //Tell ServiceStack the name and where to find your web services
+            : base("License.Manager ASP.NET Host", typeof(AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            SetConfig(new EndpointHostConfig {ServiceStackHandlerFactoryPath = "api"});
+
+            //Set JSON web services to return idiomatic JSON camelCase properties
+            ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
+
+            Plugins.Add(new CorsFeature()); //Registers global CORS Headers
+
+            RequestFilters.Add((httpReq, httpRes, requestDto) =>
+                                   {
+                                       //Handles Request and closes Responses after emitting global HTTP Headers
+                                       if (httpReq.HttpMethod == "OPTIONS")
+                                           httpRes.EndServiceStackRequest();
+                                   });
+
+            //Enable the validation feature
+            Plugins.Add(new ValidationFeature());
+
+            //This method scans the assembly for validators
+            container.RegisterValidators(typeof(AppHost).Assembly);
+
+            // register RavenDB dependencies
+            ConfigureRavenDb(container);
+
+            // register authentication framework
+            ConfigureAuthentication(container);
+        }
+
+        public void ConfigureRavenDb(Container container)
+        {
+            //NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8080);
+
+            var documentStore =
+                new Raven.Client.Embedded.EmbeddableDocumentStore()
+                    {
+                        DataDirectory = HttpContext.Current.Server.MapPath("~/App_Data/License.Manager.db/"),
+                        UseEmbeddedHttpServer = true
+                    }
+                    .Initialize();
+
+
+            IndexCreation.CreateIndexes(GetType().Assembly, documentStore);
+
+            container.Register(documentStore);
+
+            container.Register(c => c.Resolve<IDocumentStore>().OpenSession())
+                     .ReusedWithin(ReuseScope.Request);
+
+        }
+
+        private void ConfigureAuthentication(Container container)
+        {
+            var appSettings = new AppSettings();
+
+            //Default route: /auth/{provider}
+            Plugins.Add(new AuthFeature(() => new UserSession(),
+                                        new IAuthProvider[]
+                                            {
+                                                new CredentialsAuthProvider(appSettings),
+                                                //new FacebookAuthProvider(appSettings),
+                                                //new TwitterAuthProvider(appSettings),
+                                                //new BasicAuthProvider(appSettings),
+                                            }));
+
+            //Default route: /register
+            Plugins.Add(new RegistrationFeature());
+
+            container.Register<IUserAuthRepository>(c =>
+                                                    new RavenUserAuthRepository(c.Resolve<IDocumentStore>()));
+        }
+    }
+}
---
+++ src/License.Manager.Core/UserSession.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack.ServiceInterface.Auth;
+
+namespace License.Manager.Core
+{
+    public class UserSession : AuthUserSession
+    {
+        public override void OnAuthenticated(ServiceStack.ServiceInterface.IServiceBase authService, IAuthSession session, IOAuthTokens tokens, System.Collections.Generic.Dictionary<string, string> authInfo)
+        {
+            base.OnAuthenticated(authService, session, tokens, authInfo);
+        }
+    }
+}
