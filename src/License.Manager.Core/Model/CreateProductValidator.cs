65f4838d44a6fdd39b5f89882916727f3e65c528
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-22 20:57:25 +0200

    Implement validator for creating a product

---
+++ src/License.Manager.Core/Model/CreateProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack.FluentValidation;
+
+namespace License.Manager.Core.Model
+{
+    public class CreateProductValidator : AbstractValidator<CreateProduct>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.PrivateKeyPassPhrase).NotEmpty();
+        }
+    }
+}
