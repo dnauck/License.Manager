643ed549d6084905780e67eeccfa441bafa86ed7
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-22 20:50:37 +0200

    Add response DTO marker interface for service documentation

--- src/License.Manager.Core/Model/Customer.cs
+++ src/License.Manager.Core/Model/Customer.cs
@@ -5,7 +5,7 @@ namespace License.Manager.Core.Model
     [Route("/customers", "POST")]
     [Route("/customers/{Id}", "PUT, DELETE")]
     [Route("/customers/{Id}", "GET, OPTIONS")]
-    public class Customer : EntityBase
+    public class Customer : EntityBase, IReturn<Customer>
     {
         public string Name { get; set; }
         public string Company { get; set; }
