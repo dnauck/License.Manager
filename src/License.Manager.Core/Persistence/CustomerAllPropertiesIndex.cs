6c8875166ff18b123b455840b3fdf3420bcd8a1e
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-22 20:51:51 +0200

    Add RavenDB index for customer query

---
+++ src/License.Manager.Core/Persistence/CustomerAllPropertiesIndex.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using License.Manager.Core.Model;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace License.Manager.Core.Persistence
+{
+    public class CustomerAllPropertiesIndex : AbstractIndexCreationTask<Customer, CustomerAllPropertiesIndex.Result>
+    {
+        public class Result
+        {
+            public string Query { get; set; }
+        }
+
+        public CustomerAllPropertiesIndex()
+        {
+            Map = customers =>
+                  from customer in customers
+                  select new
+                             {
+                                 Query = AsDocument(customer).Select(x => x.Value)
+                             };
+
+            Index(x => x.Query, FieldIndexing.Analyzed);
+        }
+    }
+}
