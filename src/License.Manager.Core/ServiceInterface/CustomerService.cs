2d9c25d1b79ad75ad8bccd0938674c78b8ffe7cd
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-22 20:52:23 +0200

    Implemented initial customer REST service with CRUD operations

---
+++ src/License.Manager.Core/ServiceInterface/CustomerService.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Net;
+using License.Manager.Core.Model;
+using License.Manager.Core.Persistence;
+using Raven.Client;
+using ServiceStack.Common;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace License.Manager.Core.ServiceInterface
+{
+    [Authenticate]
+    public class CustomerService : ServiceStack.ServiceInterface.Service
+    {
+        private readonly IDocumentSession documentSession;
+
+        public CustomerService(IDocumentSession documentSession)
+        {
+            this.documentSession = documentSession;
+        }
+
+        public object Post(Customer customer)
+        {
+            documentSession.Store(customer);
+            documentSession.SaveChanges();
+
+            return
+                new HttpResult(customer)
+                    {
+                        StatusCode = HttpStatusCode.Created,
+                        Headers =
+                            {
+                                {HttpHeaders.Location, Request.AbsoluteUri.CombineWith(customer.Id)}
+                            }
+                    };
+        }
+
+        public object Put(Customer customer)
+        {
+            documentSession.Store(customer);
+            documentSession.SaveChanges();
+
+            return
+                new HttpResult
+                    {
+                        StatusCode = HttpStatusCode.NoContent,
+                    };
+        }
+
+        public object Delete(Customer customer)
+        {
+            documentSession.Delete(documentSession.Load<Customer>(customer.Id));
+            documentSession.SaveChanges();
+
+            return
+                new HttpResult
+                    {
+                        StatusCode = HttpStatusCode.NoContent,
+                    };
+        }
+
+        public object Get(Customer customer)
+        {
+            return documentSession.Load<Customer>(customer.Id);
+        }
+
+
+        public object Get(FindCustomers request)
+        {
+            var query = documentSession.Query<CustomerAllPropertiesIndex.Result, CustomerAllPropertiesIndex>();
+
+            if (!string.IsNullOrWhiteSpace(request.Name))
+                query = query.Search(c => c.Query, request.Name);
+
+            if (!string.IsNullOrWhiteSpace(request.Company))
+                query = query.Search(c => c.Query, request.Company);
+
+            if (!string.IsNullOrWhiteSpace(request.Email))
+                query = query.Search(c => c.Query, request.Email);
+
+            return query.OfType<Customer>().ToList();
+        }
+    }
+}
