89c42829f013ceea0125a6cb70eb84475f478729
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-22 20:56:51 +0200

    Add create product request DTO

---
+++ src/License.Manager.Core/Model/CreateProduct.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ServiceStack.ServiceHost;
+
+namespace License.Manager.Core.Model
+{
+    [Route("/products", "POST")]
+    public class CreateProduct : IReturn<Product>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string PrivateKeyPassPhrase { get; set; }
+
+        public List<string> ProductFeatures { get; set; }
+    }
+}
