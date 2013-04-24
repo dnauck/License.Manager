714167ed97da062c708a32e8b4d94917f3d7d96c
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-24 08:21:08 +0200

    Add customer validator

---
+++ src/License.Manager.Core/Model/CustomerValidator.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack.FluentValidation;
+
+namespace License.Manager.Core.Model
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Email).EmailAddress().NotEmpty();
+        }
+    }
+}
