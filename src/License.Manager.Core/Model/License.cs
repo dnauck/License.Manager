757079c2844728b5e9ae663ac23a4dd2f27569b2
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-26 17:49:01 +0200

    Implement License model

---
+++ src/License.Manager.Core/Model/License.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Portable.Licensing;
+
+namespace License.Manager.Core.Model
+{
+    public class License : EntityBase
+    {
+        public Guid LicenseId { get; set; }
+        public LicenseType LicenseType { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Expiration { get; set; }
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+        public Dictionary<string, string> ProductFeatures { get; set; }
+        public Dictionary<string, string> AdditionalAttributes { get; set; }
+    }
+}
