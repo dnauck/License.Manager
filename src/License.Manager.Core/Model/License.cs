5800e939e9a1421e5d16762d4208b29403b170c7
Daniel Nauck <d.nauck@nauck-it.de>
2013-06-11 08:03:45 +0200

    Do not reference other aggregate roots directly

--- src/License.Manager.Core/Model/License.cs
+++ src/License.Manager.Core/Model/License.cs
@@ -35,8 +35,8 @@ namespace License.Manager.Core.Model
         public LicenseType LicenseType { get; set; }
         public int Quantity { get; set; }
         public DateTime Expiration { get; set; }
-        public Customer Customer { get; set; }
-        public Product Product { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
         public Dictionary<string, string> ProductFeatures { get; set; }
         public Dictionary<string, string> AdditionalAttributes { get; set; }
     }
